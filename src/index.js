a17b1c744abc0bdf6db6f0ae67c9e0fbb4b1802d
Galiant <antonijodev@gmail.com>
2020-07-30 20:00:18 +0100

    Load images with webpack

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from "react";
 import { render } from "react-dom";
 import "./style.css";
 
-const Greeting = () => <h1>Learning Webpack 4</h1>;
+const Greeting = () => {
+  return (
+    <div>
+      <h1>Learning Webpack 4</h1>
+      <div id="image"></div>
+    </div>
+  );
+};
 
 render(<Greeting />, document.getElementById("target"));
